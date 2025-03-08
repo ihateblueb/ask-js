d533a00c26e233fb0941cf6aeb68016aefa77370
ihateblueb <ihateblueb@proton.me>
2025-03-08 18:35:59 -0500

    stats on home page

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -5,17 +5,63 @@
 	import Avatar from '$lib/components/Avatar.svelte';
 	import Loading from '$lib/components/Loading.svelte';
 	import Error from '$lib/components/Error.svelte';
+	import getMeta from '$lib/api/getMeta.js';
+	import { IconArrowBackUp, IconMessage, IconMessageReply, IconUser } from '@tabler/icons-svelte';
 
 	const query = createQuery({
 		queryKey: ['userlist'],
 		retry: false,
 		queryFn: async () => await getAllUsers()
 	});
+
+	const metaQuery = createQuery({
+		queryKey: ['meta'],
+		retry: false,
+		queryFn: async () => await getMeta()
+	});
 </script>
 
 <svelte:head>
 	<title></title>
 </svelte:head>
+
+{#if $metaQuery.isLoading}
+	<Loading />
+{:else if $metaQuery.isError}
+	<Error
+		status={$metaQuery.error.status}
+		message={$metaQuery.error.message}
+		server={Boolean($metaQuery.error.status)}
+		retry={() => $metaQuery.refetch()}
+	/>
+{:else if $metaQuery.isSuccess}
+	<div class="stats">
+		<div class="stat">
+			<div class="icon">
+				<IconUser size="18px" />
+			</div>
+			<b>{$metaQuery.data.stats.users}</b> users
+		</div>
+		<div class="stat">
+			<div class="icon">
+				<IconMessage size="18px" />
+			</div>
+			<b>{$metaQuery.data.stats.asks}</b> asks
+		</div>
+		<div class="stat">
+			<div class="icon">
+				<IconMessageReply size="18px" />
+			</div>
+			<b>{$metaQuery.data.stats.responses}</b> responses
+		</div>
+		<div class="stat">
+			<div class="icon">
+				<IconArrowBackUp size="18px" />
+			</div>
+			<b>{$metaQuery.data.stats.comments}</b> comments
+		</div>
+	</div>
+{/if}
 
 <h2>User directory</h2>
 
@@ -44,6 +90,34 @@
 {/if}
 
 <style>
+	.stats {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 10px;
+		margin-bottom: 15px;
+
+		.stat {
+			display: flex;
+			align-items: center;
+			gap: 5px;
+
+			font-size: 14px;
+			background-color: var(--bg-2);
+
+			padding: 12px;
+			width: calc(50% - 10px);
+			box-sizing: border-box;
+			border-radius: 7px;
+
+			.icon {
+				display: flex;
+				align-items: center;
+				margin-right: 2px;
+				color: var(--ac-1);
+			}
+		}
+	}
+
 	.directory {
 		margin-top: 10px;
 
