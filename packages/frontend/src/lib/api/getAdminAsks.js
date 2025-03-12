97ef1c2b760cea3158a58514d60317efc52104d8
ihateblueb <ihateblueb@proton.me>
2025-03-12 15:10:05 -0400

    fix scrolling on admin asks page

--- packages/frontend/src/lib/api/getAdminAsks.js
+++ packages/frontend/src/lib/api/getAdminAsks.js
@@ -1,6 +1,6 @@
 import https from '$lib/https';
 
-export default function getAdminAsks(id, since) {
+export default function getAdminAsks(since) {
 	return https.get(
 		`/api/v1/admin/asks` + (since ? '?since=' + since : ''),
 		true
