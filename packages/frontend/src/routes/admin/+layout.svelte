742d1a225e135c26e758f6a38c8116d7ff6f688e
ihateblueb <ihateblueb@proton.me>
2025-03-31 20:40:10 -0400

    show error on admin page to non-admin users

--- packages/frontend/src/routes/admin/+layout.svelte
+++ packages/frontend/src/routes/admin/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { page } from '$app/state';
+	import Error from '$lib/components/Error.svelte';
 	import Tab from '$lib/components/Tab.svelte';
+	import parsedLocalStore from '$lib/parsedLocalStore';
 	import {
 		IconDashboard,
 		IconMail,
@@ -8,54 +10,60 @@
 		IconSettings,
 		IconUsers
 	} from '@tabler/icons-svelte';
+
+	let isAdmin = parsedLocalStore.self?.admin ?? false;
 </script>
 
 <svelte:head>
 	<title>Admin</title>
 </svelte:head>
 
-<div class="adminTabs">
-	<div class="btnCtn noGap wrap">
-		{#key page.url.pathname}
-			<Tab
-				short
-				href={'/admin'}
-				selected={page.url.pathname === '/admin'}
-			>
-				<IconDashboard size="18px" />
-				Dashboard
-			</Tab>
-			<Tab
-				short
-				href={'/admin/invites'}
-				selected={page.url.pathname === '/admin/invites'}
-			>
-				<IconMail size="18px" />
-				Invites
-			</Tab>
-			<Tab
-				short
-				href={'/admin/users'}
-				selected={page.url.pathname === '/admin/users'}
-			>
-				<IconUsers size="18px" />
-				Users
-			</Tab>
-			<Tab
-				short
-				href={'/admin/asks'}
-				selected={page.url.pathname === '/admin/asks'}
-			>
-				<IconMessage size="18px" />
-				Asks
-			</Tab>
-		{/key}
+{#if isAdmin}
+	<div class="adminTabs">
+		<div class="btnCtn noGap wrap">
+			{#key page.url.pathname}
+				<Tab
+					short
+					href={'/admin'}
+					selected={page.url.pathname === '/admin'}
+				>
+					<IconDashboard size="18px" />
+					Dashboard
+				</Tab>
+				<Tab
+					short
+					href={'/admin/invites'}
+					selected={page.url.pathname === '/admin/invites'}
+				>
+					<IconMail size="18px" />
+					Invites
+				</Tab>
+				<Tab
+					short
+					href={'/admin/users'}
+					selected={page.url.pathname === '/admin/users'}
+				>
+					<IconUsers size="18px" />
+					Users
+				</Tab>
+				<Tab
+					short
+					href={'/admin/asks'}
+					selected={page.url.pathname === '/admin/asks'}
+				>
+					<IconMessage size="18px" />
+					Asks
+				</Tab>
+			{/key}
+		</div>
 	</div>
-</div>
 
-<div class="adminCtn">
-	<slot></slot>
-</div>
+	<div class="adminCtn">
+		<slot></slot>
+	</div>
+{:else}
+	<Error message="You do not have access to this page." retry={() => window.location.reload()} />
+{/if}
 
 <style lang="scss" scoped>
 	.adminTabs {
